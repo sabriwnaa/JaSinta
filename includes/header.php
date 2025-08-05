526b1170d2899f8f470ed6aaa87b6332e9169b32
sabriwnaa <sabrinahm2006@gmail.com>
2025-08-04 21:41:01 -0300

    [new] first commit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    
+        <div class='header'>
+            <h1 class='titulo'>JaSinta</h1>
+            <p class='texto'>Guie-se pelo sentir</p>
+        </div>
+        
+</body>
+</html>
