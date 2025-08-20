3796be8255a42ac17e737cf2061d059636c27ae1
SabGab <sabrinahm2006@gmail.com>
2025-08-20 11:58:04 -0300

    [upd] tudo no mesmo css, porém organizado e ainda com algumas coisas para ajustar

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JaSinta</title>
     <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;700&family=Inter:wght@400;700&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">  
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
 
 </head>
--- modulos/jasinta.sql
+++ modulos/jasinta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/08/2025 às 03:55
+-- Tempo de geração: 12/08/2025 às 14:52
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -34,6 +34,16 @@ CREATE TABLE `categoria` (
   `nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'pet'),
+(2, 'pessoa'),
+(3, 'paisagem'),
+(4, 'arte');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +55,28 @@ CREATE TABLE `confianca` (
   `down_pontuacao` int(11) NOT NULL,
   `up_pontuacao` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `confianca`
+--
+
+INSERT INTO `confianca` (`id`, `down_pontuacao`, `up_pontuacao`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 2, 1),
+(6, 2, 2),
+(7, 2, 3),
+(8, 2, 4),
+(9, 3, 1),
+(10, 3, 2),
+(11, 3, 3),
+(12, 3, 4),
+(13, 4, 1),
+(14, 4, 2),
+(15, 4, 3),
+(16, 4, 4);
 
 -- --------------------------------------------------------
 
@@ -58,8 +90,35 @@ CREATE TABLE `foto` (
   `descricao` varchar(255) NOT NULL,
   `jogadora` int(11) NOT NULL,
   `is_ativa` tinyint(4) NOT NULL DEFAULT 1,
-  `is_preferencia` tinyint(4) NOT NULL DEFAULT 0
+  `is_preferencia` tinyint(4) NOT NULL DEFAULT 0,
+  `data_cadastro` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `foto`
+--
+
+INSERT INTO `foto` (`id`, `imagem`, `descricao`, `jogadora`, `is_ativa`, `is_preferencia`, `data_cadastro`) VALUES
+(21, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(22, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(23, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(24, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(25, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(26, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(27, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(28, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(29, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(30, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(31, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(32, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(33, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(34, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(35, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(36, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(37, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(38, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(39, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12'),
+(40, 'img/image1.png', 'esta é a historia dessa imagem', 1, 1, 0, '2025-08-12');
 
 -- --------------------------------------------------------
 
@@ -72,6 +131,16 @@ CREATE TABLE `foto_categoria` (
   `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `foto_categoria`
+--
+
+INSERT INTO `foto_categoria` (`id_foto`, `id_categoria`) VALUES
+(21, 1),
+(22, 2),
+(23, 3),
+(24, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -80,8 +149,19 @@ CREATE TABLE `foto_categoria` (
 
 CREATE TABLE `jogadora` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
+  `nome` varchar(255) NOT NULL,
+  `data_cadastro` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `jogadora`
+--
+
+INSERT INTO `jogadora` (`id`, `nome`, `data_cadastro`) VALUES
+(1, 'Sabrina', '2025-08-12'),
+(2, 'Isabela', '2025-08-12'),
+(3, 'Vanitas', '2025-08-12'),
+(4, 'Matias', '2025-08-12');
 
 -- --------------------------------------------------------
 
@@ -91,8 +171,92 @@ CREATE TABLE `jogadora` (
 
 CREATE TABLE `sentimento` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
+  `nome` varchar(255) NOT NULL,
+  `data_cadastro` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `sentimento`
+--
+
+INSERT INTO `sentimento` (`id`, `nome`, `data_cadastro`) VALUES
+(1, 'Admiração', '2025-08-12'),
+(2, 'Carinho', '2025-08-12'),
+(3, 'Amizade', '2025-08-12'),
+(4, 'Alegria', '2025-08-12'),
+(5, 'Felicidade', '2025-08-12'),
+(6, 'Alívio', '2025-08-12'),
+(7, 'Esperança', '2025-08-12'),
+(8, 'Encantamento', '2025-08-12'),
+(9, 'Amor', '2025-08-12'),
+(10, 'Gratidão', '2025-08-12'),
+(11, 'Orgulho', '2025-08-12'),
+(12, 'Saudade', '2025-08-12'),
+(13, 'Empolgação', '2025-08-12'),
+(14, 'Euforia', '2025-08-12'),
+(15, 'Satisfação', '2025-08-12'),
+(16, 'Ternura', '2025-08-12'),
+(17, 'Paz', '2025-08-12'),
+(18, 'Segurança', '2025-08-12'),
+(19, 'Liberdade', '2025-08-12'),
+(20, 'Cumplicidade', '2025-08-12'),
+(21, 'Tristeza', '2025-08-12'),
+(22, 'Medo', '2025-08-12'),
+(23, 'Raiva', '2025-08-12'),
+(24, 'Ciúmes', '2025-08-12'),
+(25, 'Frustração', '2025-08-12'),
+(26, 'Culpa', '2025-08-12'),
+(27, 'Vergonha', '2025-08-12'),
+(28, 'Inveja', '2025-08-12'),
+(29, 'Desespero', '2025-08-12'),
+(30, 'Rejeição', '2025-08-12'),
+(31, 'Solidão', '2025-08-12'),
+(32, 'Solitude', '2025-08-12'),
+(33, 'Desconfiança', '2025-08-12'),
+(34, 'Humilhação', '2025-08-12'),
+(35, 'Angústia', '2025-08-12'),
+(36, 'Melancolia', '2025-08-12'),
+(37, 'Desamparo', '2025-08-12'),
+(38, 'Ansiedade', '2025-08-12'),
+(39, 'Nostalgia', '2025-08-12'),
+(40, 'Surpresa', '2025-08-12'),
+(41, 'Vulnerabilidade', '2025-08-12'),
+(42, 'Confusão', '2025-08-12'),
+(43, 'Curiosidade', '2025-08-12'),
+(44, 'Exaustão', '2025-08-12'),
+(45, 'Apego', '2025-08-12'),
+(46, 'Tensão', '2025-08-12'),
+(47, 'Desequilíbrio', '2025-08-12'),
+(48, 'Inquietação', '2025-08-12'),
+(49, 'Silêncio', '2025-08-12'),
+(50, 'Flutuação', '2025-08-12'),
+(51, 'Deslumbramento', '2025-08-12'),
+(52, 'Clariciano', '2025-08-12'),
+(53, 'Incômodo', '2025-08-12'),
+(54, 'Incompletude', '2025-08-12'),
+(55, 'Entorpecimento', '2025-08-12'),
+(56, 'Ilusão', '2025-08-12'),
+(57, 'Fuga', '2025-08-12'),
+(58, 'Encontro', '2025-08-12'),
+(59, 'Queda', '2025-08-12'),
+(60, 'Retorno', '2025-08-12'),
+(61, 'Recomeço', '2025-08-12'),
+(62, 'Perda', '2025-08-12'),
+(63, 'Invisibilidade', '2025-08-12'),
+(64, 'Vazio', '2025-08-12'),
+(65, 'Tédio', '2025-08-12'),
+(66, 'Serenidade', '2025-08-12'),
+(67, 'Contemplação', '2025-08-12'),
+(68, 'Contentamento', '2025-08-12'),
+(69, 'Calmaria', '2025-08-12'),
+(70, 'Compaixão', '2025-08-12'),
+(71, 'Confiança', '2025-08-12'),
+(72, 'Insegurança', '2025-08-12'),
+(73, 'Fúria', '2025-08-12'),
+(74, 'Êxtase', '2025-08-12'),
+(75, 'Rebeldia', '2025-08-12'),
+(76, 'Justiça', '2025-08-12'),
+(77, 'Injustiça', '2025-08-12');
 
 --
 -- Índices para tabelas despejadas
@@ -144,31 +308,31 @@ ALTER TABLE `sentimento`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `confianca`
 --
 ALTER TABLE `confianca`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT de tabela `jogadora`
 --
 ALTER TABLE `jogadora`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `sentimento`
 --
 ALTER TABLE `sentimento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- Restrições para tabelas despejadas
